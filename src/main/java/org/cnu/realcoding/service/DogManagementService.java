8e829e82facbcdbcc5ef5b814d1061e49ac66d44
k906506 <k906506@naver.com>
2021-04-07 00:29:40 +0900

    Service 수정

--- src/main/java/org/cnu/realcoding/service/DogManagementService.java
+++ src/main/java/org/cnu/realcoding/service/DogManagementService.java
@@ -20,12 +20,40 @@ public class DogManagementService {
         //return ;
     }
 
-    public Dog getDogByName(String name) {
-        Dog dog = dogRepository.findDog(name);
-
-        if(dog == null){
-            // throw exception
+    public Dog getDogByName(String name) throws DogNotFoundException { // name만을 파라미터로 받는 api
+        Dog dog = dogRepository.findDogFromName(name);
+        if (dog == null){
+            throw new DogNotFoundException();
         }
         return dog;
     }
+
+    public Dog getDogByOwnerName(String ownerName) throws DogNotFoundException { // ownerName만을 파라미터로 받는 api
+        Dog dog = dogRepository.findDogFromOwnerName(ownerName);
+        if (dog == null){
+            throw new DogNotFoundException();
+        }
+        return dog;
+    }
+
+    public Dog getDogByOwnerPhoneNumber(String ownerPhoneNumber) throws DogNotFoundException {  // ownerPhoneNumber만을 파라미터로 받는 api
+        Dog dog = dogRepository.findDogFromOwnerPhoneNumber(ownerPhoneNumber);
+        if (dog == null){
+            throw new DogNotFoundException();
+        }
+        return dog;
+    }
+
+    public Dog getDogByAllParameter(String name, String ownerName, String ownerPhoneNumber) throws DogNotFoundException {  // 세 가지 파라미터를 모두 파라미터로 받는 api
+        Dog dog = dogRepository.findDogFromOwnerPhoneNumber(ownerPhoneNumber);
+        if (dog == null){
+            throw new DogNotFoundException();
+        }
+        return dog;
+    }
+
+
+
+
+
 }
