ff7d007f4aad6c374f5c0c325bd9c48ebd9682ac
k906506 <k906506@naver.com>
2021-04-07 00:28:49 +0900

    Controller 수정

--- src/main/java/org/cnu/realcoding/controller/DogController.java
+++ src/main/java/org/cnu/realcoding/controller/DogController.java
@@ -2,16 +2,18 @@ package org.cnu.realcoding.controller;
 
 import org.cnu.realcoding.domain.Dog;
 import org.cnu.realcoding.service.DogManagementService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
 public class DogController {
 
+    @Autowired
     private DogManagementService dogManagementService;
 
+    @PostMapping("/dogs")
     public void createDog(@RequestBody Dog dog){
         dogManagementService.insertDog(dog);
     }
