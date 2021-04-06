d92e07cf0703b22ad8243ed885dd47b1fb3b525e
Dongheon Lee <jdd04063@gmail.com>
2021-04-06 22:19:36 +0900

    add controller, repository, service

---
+++ src/main/java/org/cnu/realcoding/controller/DogController.java
@@ -0,0 +1,29 @@
+package org.cnu.realcoding.controller;
+
+import org.cnu.realcoding.domain.Dog;
+import org.cnu.realcoding.service.DogManagementService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+public class DogController {
+
+    private DogManagementService dogManagementService;
+
+    public void createDog(@RequestBody Dog dog){
+        dogManagementService.insertDog(dog);
+    }
+
+    @GetMapping("/dogs")
+    public List<Dog> getAllDogs(){
+        return dogManagementService.getAllDogs();
+    }
+
+    // localhost:8011/dogs
+    public Dog getDogByName(@PathVariable String name){
+        return dogManagementService.getDogByName(name);
+    }
+
+}
