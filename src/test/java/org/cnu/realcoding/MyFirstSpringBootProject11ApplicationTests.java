7aee904213b691fb4b8900931871a2fe0f4743dd
k906506 <k906506@naver.com>
2021-04-06 21:25:10 +0900

    11조

---
+++ src/test/java/org/cnu/realcoding/MyFirstSpringBootProject11ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cnu.realcoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSpringBootProject11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
