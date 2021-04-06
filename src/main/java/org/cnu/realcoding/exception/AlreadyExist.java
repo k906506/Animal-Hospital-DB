7945ef8cd04e75e610008ac0d84d78c6b693a7dd
201702083 <201702083@o.cnu.ac.kr>
2021-04-07 00:16:20 +0900

    주석 작성

--- src/main/java/org/cnu/realcoding/exception/AlreadyExist.java
+++ src/main/java/org/cnu/realcoding/exception/AlreadyExist.java
@@ -3,6 +3,6 @@ package org.cnu.realcoding.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class DogNotFoundException extends RuntimeException{
+@ResponseStatus(HttpStatus.CONFLICT)
+public class AlreadyExist extends RuntimeException{ // 이미 존재하는 데이터가 있을 경우 에러
 }
--- src/main/java/org/cnu/realcoding/exception/DogNotFoundException.java
+++ src/main/java/org/cnu/realcoding/exception/DogNotFoundException.java
@@ -4,5 +4,5 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class DogNotFoundException extends RuntimeException{
+public class DogNotFoundException extends RuntimeException{ // 데이터에 존재하지 않을 경우 에러
 }
